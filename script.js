7b5a44a4f8b036ae9e499bc8ec0b484fb9539b83
choko1229 <chokops4twitter@gmail.com>
2023-11-29 18:18:55 +0900

    フッターの色変更

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let leaving = false;
 // ページ離脱時の処理
 window.addEventListener('beforeunload', function(event) {
   leaving = true;
-  document.body.classList.add('leaving');
+  document.querySelector('footer').classList.add('leaving');
 });
 
 // ページ再訪問時の処理
 window.addEventListener('load', function(event) {
   if (leaving) {
-    document.body.classList.add('leaving');
+    document.querySelector('footer').classList.add('leaving');
   }
 });
